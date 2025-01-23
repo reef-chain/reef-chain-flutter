b5a6f5bfbbb41af266c1d063d759bcd4852e64a0
matjazonline <matjazonline@gmail.com>
2025-01-23 11:16:37 +0100

    v0.0.3

--- lib/js_api_service.dart
+++ lib/js_api_service.dart
@@ -8,7 +8,7 @@ import 'package:rxdart/rxdart.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 // Import for Android features.
-import 'package:webview_flutter_android/webview_flutter_android.dart';
+// import 'package:webview_flutter_android/webview_flutter_android.dart';
 
 // Import for iOS/macOS features.
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
